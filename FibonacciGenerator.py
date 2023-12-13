a7cf9d1a07d6bfcdf841a7a8feb545b5a8682f1f
Angel <143033010+TheArchangelWarrior@users.noreply.github.com>
2023-12-13 11:14:48 -0600

    Create FibonacciGenerator.py

---
+++ FibonacciGenerator.py
@@ -0,0 +1,15 @@
+#Made by TheArchangelWarrior on github
+
+nterms = int(input("Enter the number of terms"))
+n1, n2 = 0, 1
+count = 0
+
+if nterms == None && nterms <= 0:
+  print("Please use a valide intager that is higher than 0")
+else:
+  while count < nterms:
+    print(n1)
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    count += 1
